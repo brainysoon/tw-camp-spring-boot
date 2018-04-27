e6ea739165ed77a724643c1b28410fb3889aef2c
brainysoon <brainysoon@gmail.com>
2018-04-27 09:38:38 +0800

    [ Project ] - Add Employee domain object.

---
+++ src/main/java/com/thoughtworks/fireman/domain/Employee.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.fireman.domain;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private int id;
+    private String name;
+    private int age;
+    private int gender;
+}
