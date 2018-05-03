e8812b71e57391af60f4bd9b86a3d984cbb76d6f
brainysoon <brainysoon@gmail.com>
2018-05-03 22:43:35 +0800

    [ Project ] - Change spring mvc to match view.

--- src/main/java/com/thoughtworks/fireman/Application.java
+++ src/main/java/com/thoughtworks/fireman/Application.java
@@ -2,11 +2,18 @@ package com.thoughtworks.fireman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
